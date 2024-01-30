f468cf21ed51741edef07d708025daa07ae31aa1
Laderlappenn <ciphra5@mail.ru>
2024-01-30 10:51:22 +0600

    user model, specialist model

--- MainPageApp/views.py
+++ MainPageApp/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    print("test")
     return (render(request, "MainPageApp/index.html",))
--- OrderSpecialistApp/models.py
+++ OrderSpecialistApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
+from ProfileApp.models import User
 # Create your models here.
+
+
+class Specialist(User):
+    SPECIALIZATION_CHOICES = {
+        "PLUMBER": "Plumber",
+        "ELECTRICIAN": "Electrician",
+    }
+
+    specialization = models.CharField(max_lenght=20,
+                                      choices=SPECIALIZATION_CHOICES)
+
+
+class Rating(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    changed_at = models.DateTimeField(auto_now=True)
