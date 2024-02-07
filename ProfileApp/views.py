d50d173364aa5c889d73404043148584d14eb87a
Laderlappenn <ciphra5@mail.ru>
2024-02-07 18:55:26 +0600

    profile

--- ProfileApp/views.py
+++ ProfileApp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from ProfileApp.models import Profile
-from django.http import JsonResponse
-from SpecialistApp.forms import SpecialistRegistrationForm
-from ProfileApp.forms import ProfileRegistrationForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+
+from ProfileApp.models import Profile
+from ProfileApp.forms import ProfileRegistrationForm, LoginForm
+from SpecialistApp.forms import SpecialistRegistrationForm
 
 
 @login_required
@@ -15,12 +15,14 @@ def profile(request, id: int = None):
     if id is None:
         id = request.user.id
     profile_obj = get_object_or_404(QueryS, id=id)
-    return JsonResponse({'test': profile_obj.email})
+    return render(request, "ProfileApp/profile.html",{'profile': profile_obj})
+
 
 def register(request):
     profile_form = ProfileRegistrationForm()
     specialist_form = SpecialistRegistrationForm()
     return render(request, "register.html", {'profile_form': profile_form, 'specialist_form': specialist_form})
+
 
 def register_profile(request):
     if request.method == "POST":
@@ -28,17 +30,18 @@ def register_profile(request):
         if form.is_valid():
             form.save()
 
+
 def register_specialist(request):
     if request.method == "POST":
         form = SpecialistRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
 
+
 def login_profile(request):
     if request.method == "POST":
         login_form = LoginForm(data=request.POST) # why data required??
         if login_form.is_valid():
-            print(login_form.cleaned_data)
             email = login_form.cleaned_data['username'] #TODO replace username with email
             password = login_form.cleaned_data['password']
             user = authenticate(email=email, password=password)
@@ -50,6 +53,7 @@ def login_profile(request):
     else:
         login_form = LoginForm()
         return render(request, "login.html", {'login_form': login_form})
+
 
 def logout_profile(request):
     logout(request)
