b42d64afdc4479c81e3f67ba168c0010185429c8
Laderlappenn <ciphra5@mail.ru>
2024-02-27 23:07:50 +0600

    page visits chart

--- StatisticApp/views.py
+++ StatisticApp/views.py
@@ -11,6 +11,6 @@ def specialist_ad_visits_statistic(request):
         specialist_id = request.user.specialist.id # no n+1 problem because of new backend          
         ads = Ad.objects.filter(specialist_fk=specialist_id).values("id") # i don't like this method
         ad_paths = [f"/ad/{ads[ad_id]["id"]}/" for ad_id in range(len(ads))]
-        visits = Visits.objects.filter(path__in=ad_paths)
+        visits = Visits.objects.filter(path__in=ad_paths).order_by("-count")
         return render(request, "StatisticApp/ad_statistic.html", {"ad_visits": visits})
     return HttpResponseRedirect(reverse("main_page:index"))  #TODO add no access page
