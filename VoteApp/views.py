fac0249dd52f9d3e59a80ea7e5c2d8c45f307ec3
Laderlappenn <ciphra5@mail.ru>
2024-03-22 19:49:41 +0500

    upd

--- VoteApp/views.py
+++ VoteApp/views.py
@@ -7,13 +7,11 @@ from .models import Vote
 @login_required
 def ad_vote(request, ad_id):
     user_id = request.user.id
-    print(request.META.get("HTTP_VOTED"))
     if request.META.get("HTTP_VOTED") == "true":
         voted = 1
     else:
         voted = 0
     vote, created = Vote.objects.update_or_create(ad_id=ad_id, user_id=user_id, defaults={'vote': voted},)
-    print(vote.vote)
     if created:
         return HttpResponse(status=201)
     return HttpResponse(status=200)
